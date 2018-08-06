b08c9eb6bf4454b137583fc3be959f58829e4ed9
mistercmd <carson.deru@gmail.com>
2018-08-06 09:02:37 -0600

    built tab nav structure

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,13 +12,16 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.de
 // import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
+import history from './history';
+
 import Layout from './components/layout';
 
+import requireAuth from './components/requireAuth';
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
 
-import history from './history';
-import requireAuth from './components/requireAuth';
+
+import dashboard from './components/dashboard';
 
 
 
---
+++ src/components/dashboard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import TavNav from './tabnav';
+
+
+class Dashboard extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tabs: [
+                {
+                    title: 'Newsletter',
+                    active: false,
+                    component: <h4>Hey There - Newsletter</h4>
+                },
+                {
+                    title: 'Requests',
+                    active: false,
+                    component: <h4>Hey There - Requests</h4>
+                },
+                
+
+            ]
+        }
+    }
+   render() {
+      return (
+         <TabNav tabs={this.state.tabs}/>
+      )
+   }
+}
+
+export default Dashboard;
